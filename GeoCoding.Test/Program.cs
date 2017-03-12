4e85590221f76d9a112690879bffe6fb9d7d54a2
Rénald <Rnald@RVV-8>
2017-03-12 18:48:44 +0100

    Refacto...

--- GeoCoding.Test/Program.cs
+++ GeoCoding.Test/Program.cs
@@ -8,7 +8,7 @@ using Geocoding.Google;
 
 namespace GeoCoding.Test
 {
-    internal class Program
+    public class Program
     {
         private static void GenerateFichierPoi(IEnumerable<GoogleAddress> addresses, string date)
         {
